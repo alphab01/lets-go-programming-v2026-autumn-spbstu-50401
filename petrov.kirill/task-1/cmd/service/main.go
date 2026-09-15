86f46cc1e69dcebafbbc57f64be806a1e64e34a2
alphabo1 <kp1682674@gmail.com>
2026-09-15 05:44:30 +0300

    [TASK-1] adding base funcs

--- petrov.kirill/task-1/cmd/service/main.go
+++ petrov.kirill/task-1/cmd/service/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("hello world")
+  var a, b int
+  fmt.Scan(&a, &b)
+  var s string
+  fmt.Scan(&s)
+  if (s == "+") {
+    fmt.Println(a + b)
+  } else if (s == "-") {
+    fmt.Println(a - b)
+  } else if (s == "/") {
+    fmt.Println(a / b)
+  } else {
+    fmt.Println(a * b)
+  }
 }
