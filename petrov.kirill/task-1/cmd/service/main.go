0fc6de9ea414b540fbe43da2f1f65839d2b24bce
alphabo1 <kp1682674@gmail.com>
2026-09-15 05:51:37 +0300

    [TASK-1] adding operand check

--- petrov.kirill/task-1/cmd/service/main.go
+++ petrov.kirill/task-1/cmd/service/main.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func main() {
   var a, b int
-  fmt.Scan(&a, &b)
+  _, err := fmt.Scan(&a)
+  if (err != nil) {
+    fmt.Println("Invalid first operand")
+    return
+  }
+  _, err = fmt.Scan(&b)
+  if (err != nil) {
+    fmt.Println("Invalid second operand")
+    return
+  }
   var s string
   fmt.Scan(&s)
   if (s == "+") {
